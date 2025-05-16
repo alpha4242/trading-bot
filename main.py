29775e3e5b2046c7e099dcf495ff194095d5ffb3
Aliv Mishra <121473619+alpha4242@users.noreply.github.com>
2025-05-16 09:07:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ API_KEY = os.getenv("BYBIT_API_KEY")
 API_SECRET = os.getenv("BYBIT_API_SECRET")
 
 symbol = 'PNUTUSDT'
-timeframe = '1m'
+timeframe = '5m'
 ema_short_period = 9
 ema_long_period = 21
 quantity = 6  # Position size per trade
